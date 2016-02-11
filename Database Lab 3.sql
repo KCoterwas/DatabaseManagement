1cf79ee381de2a0ffa1d31c555bc57c779afd4c1
Kim Coterwas <Kimberly.Coterwas1@marist.edu>
2016-02-10 21:04:39 -0500

    Completed Lab 3

---
+++ Database Lab 3.sql
@@ -0,0 +1,42 @@
+-- 1. List the ordno and dollars of all orders 
+SELECT ordnum, totalUSD 
+FROM orders; 
+
+-- 2. List the name and city of agents named Smith 
+SELECT city, name  
+FROM agents;  
+
+-- 3. List the pid, name, and price USD of products with quantity more than 208,000 
+SELECT pid, name, priceUSD, quantity 
+FROM products  
+WHERE quantity > 208000; 
+
+-- 4. List the names and cities of customers in Dallas 
+SELECT name, city 
+FROM customers  
+WHERE city = 'Dallas'; 
+
+-- 5. List the names of agents not in New York and not in Tokyo 
+SELECT name  
+FROM agents 
+WHERE city NOT IN ('New York', 'Tokyo');  
+
+-- 6. List all data for products not in Dallas or Duluth that cost US$1 or more 
+SELECT * 
+FROM products  
+WHERE city NOT IN ('Dallas', 'Duluth') AND priceUSD >= 1; 
+
+-- 7. List all data for orders in January or March 
+SELECT *  
+FROM orders 
+WHERE mon IN ('jan', 'mar');  
+
+-- 8. List all data for orders in February less than us$500 
+SELECT * 
+FROM orders  
+WHERE mon IN ('feb') AND totalUSD < 500; 
+
+-- 9. List all orders from the customer whose cid is C005 
+SELECT * 
+FROM customers  
+WHERE cid = 'c005'; 
