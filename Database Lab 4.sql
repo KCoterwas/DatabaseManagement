6d271a42fb4a939925bbffb62bf79babf798c70b
Kim Coterwas <Kimberly.Coterwas1@marist.edu>
2016-02-17 08:17:32 -0500

    Worked on a few questions
    Questions 1-3 done

--- Database Lab 4.sql
+++ Database Lab 4.sql
@@ -11,28 +11,48 @@ WHERE aid in (SELECT aid
 -- customer in Dallas, sorted by pid from highest to lowest. (This is not the same as asking 
 -- for ids of products ordered by customers in Dallas.)
 
-
+SELECT pid
+FROM orders
+WHERE aid IN (SELECT aid
+              FROM orders
+              WHERE cid IN (SELECT cid
+       			    FROM customers
+       			    WHERE city = 'Dallas'));
 
 -- 3. Get the ids and names of customers who did not place an order through agent a01.
 
-
+SELECT cid, name
+FROM customers
+WHERE cid NOT IN (SELECT cid
+       	      	  FROM orders
+     	      	  WHERE aid IN ('a01'));
 
 -- 4. Get the ids of customers who ordered both product p01 and p07.
 
-
+SELECT cid
+FROM orders
+WHERE pid IN (SELECT pid
+       FROM products
+       WHERE pid = 'p01' AND pid = 'p07');
 
 -- 5. Get the ids of products not ordered by any customers who placed any order through agent
 -- a07 in pid order from highest to lowest.
 
-
+SELECT 
+FROM 
+WHERE
 
 -- 6. Get the name, discounts, and city for all customers who place orders through agent a07
 -- in pid order from highest to lowest.
 
-
+SELECT 
+FROM 
+WHERE
 
 -- 7. Get all customers who have the same discount as that of any customers in Dallas or London.
 
-
+SELECT 
+FROM 
+WHERE
 
 -- 8. ESSAY ON CHECK CONSTRAINTS
