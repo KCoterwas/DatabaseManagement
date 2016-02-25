1725239c009fc9138c7e3ff3f59fdada98db65f5
Kim Coterwas <Kimberly.Coterwas1@marist.edu>
2016-02-24 20:51:57 -0500

    Completed lab

--- Database Lab 5.sql
+++ Database Lab 5.sql
@@ -1,25 +1,45 @@
+--Kim Coterwas
 --Lab 5
+
 --1. Show the cities of agents booking an order for a customer whose id is 'c002'. 
 --   Use joins; no subqueries.
 
-
+SELECT a.city
+FROM agents a, orders o
+WHERE a.aid = o.aid
+AND o.cid = 'c002';
 
 --2. Show the ids of products ordered through any agent who makes at least one order
 --   for a customer in Dallas, sorted by pid from highest to lowest. Use joins; no subqueries
 
-
+SELECT DISTINCT o.pid
+FROM orders o, customers c
+WHERE o.cid = c.cid
+AND c.city = 'Dallas'
+ORDER BY pid DESC;
 
 --3. Show the names of customers who have never placed an order. Use a subquery.
 
-
+SELECT name
+FROM customers
+WHERE cid NOT IN (SELECT cid
+		  FROM orders);
 
 --4. Show the names of customers who have never placed an order. Use an outer join.
 
-
+SELECT c.name
+FROM customers c
+LEFT OUTER JOIN orders o ON c.cid = o.cid
+WHERE o.ordno IS NULL;
 
 --5. Show the names of customers who placed at least one order through an agent in their
 --   own city, along with those agent(s') names.
 
+SELECT DISTINCT c.name, a.name
+FROM orders o
+INNER JOIN agents a ON o.aid = a.aid
+INNER JOIN customers c ON o.cid = c.cid
+WHERE c.city = a.city;
 
 
 --6. Show the names of customers and agents living in the same city, along with the name 
@@ -35,8 +55,8 @@ WHERE c.city = a.city;
 
 SELECT c.name, c.city
 FROM customers c
-WHERE c.city IN (SELECT name, city
+WHERE c.city IN (SELECT city
 		 FROM products
 		 GROUP BY city
-		 ORDER BY cpid(pid) ASC
+		 ORDER BY count(pid) ASC
 		 LIMIT 1);
