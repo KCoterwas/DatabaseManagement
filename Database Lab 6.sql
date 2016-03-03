6ab371a3370f868b2023a92f1c76e5e3dea8f48e
Kim Coterwas <Kimberly.Coterwas1@marist.edu>
2016-03-02 20:27:01 -0500

    Did questions 2 and 3

--- Database Lab 6.sql
+++ Database Lab 6.sql
@@ -10,12 +10,19 @@
 -- 2. Display the names of products whose priceUSD is strictly above the average 
 --    priceUSD, in reverse-alphabetical order.
 
-
+SELECT name
+FROM products
+WHERE priceUSD > (SELECT Avg(priceUSD)
+      		  FROM products)
+ORDER BY name DESC;
 
 -- 3. Display customer name, pid ordered, and the total for all orders, sorted by 
 --    total from high to low.
 
-
+SELECT c.name, o.pid, o.dollars
+FROM orders o
+INNER JOIN customers c ON c.cid = o.cid
+ORDER BY o.dollars DESC;
 
 -- 4. Display all customer names (in alphabetical order) and their total ordered, 
 --    and nothing more. Use coalesce to avoid showing NULLs.
@@ -34,10 +41,10 @@
 --    Orders.dollars is incorrect, if any.
 
 SELECT *
-FROM (Select o.*, o.qty*p.priceusd*(1-(discount/100)) as truedollars
+FROM (SELECT o.*, o.qty*p.priceUSD*(1-(discount/100)) AS truedollars
       FROM orders o
       INNER JOIN products p on o.pid = p.pid
-      INNER JOIN customers c on o.cid = c.cid) as tmptable
+      INNER JOIN customers c on o.cid = c.cid) AS tmptable
 WHERE dollars != truedollars
 
 -- 7. What is the difference between a LEFT OUTER JOIN and a RIGHT OUTER JOIN? Give 
